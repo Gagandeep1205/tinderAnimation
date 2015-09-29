ba1f7f3c78e28e28f3eb9440d8a4728044d796d2
Gagandeep Kaur <Gagan@Gagandeeps-Mac-mini.local>
2015-09-29 15:57:46 +0530

    Initial Commit

---
+++ TinderAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinderAnimation
+//
+//  Created by Gagandeep Kaur  on 29/09/15.
+//  Copyright (c) 2015 Gagandeep Kaur . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinderAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinderAnimation
+//
+//  Created by Gagandeep Kaur  on 29/09/15.
+//  Copyright (c) 2015 Gagandeep Kaur . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
